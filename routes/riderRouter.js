2afcf3c9689e83fa1879684f5ccf80ad6fb44d78
jbb-dev <jbbouillat@yahoo.fr>
2020-07-17 14:33:54 +0200

    riderRouter and modif on riderModel

--- routes/riderRouter.js
+++ routes/riderRouter.js
@@ -36,8 +36,8 @@ riderRouter.get('/search/?', (req,res) => {
   const level = Number(req.query.level) || 0
   const vehiculed = req.query.vehiculed
   const budget = req.query.budget || 0
-  const competition = req.query.competition
-  const getLesson = req.query.lesson
+  const competition = req.query.competition || ''
+  const managed = req.query.managed
   const getYears = Number(req.query.years) || 0 
   const disciplines = req.query.disciplines || ''
   const ridingFrequency = req.query.frequency || ''
@@ -54,13 +54,13 @@ riderRouter.get('/search/?', (req,res) => {
       rider_gallop_level : {[Op.gte] : level }, // Soit le level est précisé, sinon on prend tous les résultats
       rider_vehiculed : vehiculed || {[Op.or] : [0,1]}, // Soit l'information est précisée, soit on propose tous les cavaliers, peu importe si véhiculé ou non
       rider_budget : { [Op.gte] : budget }, //
-      rider_competition : competition || {[Op.or] : [0,1]}, // soit non, soit obligatoire, soit possible
-      rider_get_lessons : getLesson || {[Op.or] : [0,1]}, // non 
+      rider_competition : {[Op.like] : competition}, // soit non, soit obligatoire, soit possible
+      // rider_get_lessons : getLesson || {[Op.or] : [0,1]}, // non 
       rider_years_of_practice : {[Op.gte] : getYears },
-      rider_disciplines : {[Op.like] : disciplines}, // si renseignée, alors uniquement les résultats avec une discipline, sinon tous les résultats
+      // rider_disciplines : {[Op.like] : disciplines}, // si renseignée, alors uniquement les résultats avec une discipline, sinon tous les résultats
       rider_riding_frequency : {[Op.like] : ridingFrequency },
       rider_fixed_day : regularity || {[Op.or] : [0,1]},
-      rider_agree_other_discipline : other_discipline || {[Op.or] : [0,1]}
+      // rider_agree_other_discipline : other_discipline || {[Op.or] : [0,1]}
     },
 
     include : [models.Ideal_horse]
