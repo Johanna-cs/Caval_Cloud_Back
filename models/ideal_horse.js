1789091b70eb6094e0684546787804ee967c5fcd
nadir-13016 <nadir-13016@hotmail.fr>
2020-07-29 10:37:28 +0200

    commit pour le kiff

--- models/ideal_horse.js
+++ models/ideal_horse.js
@@ -1,48 +1,48 @@
-// 'use strict';
+'use strict';
 
-// module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
 
-//     const Ideal_horse = sequelize.define('Ideal_horse', {
+    const Ideal_horse = sequelize.define('Ideal_horse', {
 
-//         ideal_horse_ID : {
-//             type: DataTypes.INTEGER,
-//             autoIncrement: true,
-//             primaryKey: true,  
-//         },
+        ideal_horse_ID : {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,  
+        },
 
-//         ideal_horse_size: {
-//             type : DataTypes.INTEGER,
-//         },
+        ideal_horse_size: {
+            type : DataTypes.INTEGER,
+        },
 
-//         ideal_horse_age: {
-//             type : DataTypes.INTEGER,
-//         },
+        ideal_horse_age: {
+            type : DataTypes.INTEGER,
+        },
 
-//         ideal_horse_temper: {
-//             type : DataTypes.STRING,
-//             validate : {
-//                 max : 80
-//             }
-//         },
+        ideal_horse_temper: {
+            type : DataTypes.STRING,
+            validate : {
+                max : 80
+            }
+        },
 
-//         ideal_horse_caracter: {
-//             type : DataTypes.STRING,
-//             validate : {
-//                 max : 80
-//             }
-//         },
+        ideal_horse_caracter: {
+            type : DataTypes.STRING,
+            validate : {
+                max : 80
+            }
+        },
 
-//         ideal_horse_body_type: {
-//             type : DataTypes.STRING,
-//             validate : {
-//                 max : 80
-//             }
-//         },
-//     }, {});
+        ideal_horse_body_type: {
+            type : DataTypes.STRING,
+            validate : {
+                max : 80
+            }
+        },
+    }, {});
 
-//     Ideal_horse.associate= models =>{
-//         Ideal_horse.hasMany(models.Rider)
-//     }
+    Ideal_horse.associate= models =>{
+        Ideal_horse.hasMany(models.Rider)
+    }
     
-//     return Ideal_horse;
-// }
+    return Ideal_horse;
+}
